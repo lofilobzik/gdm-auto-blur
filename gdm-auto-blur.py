b6d22ae0026eb097c9ca26a88e2ade3c8291df9e
lofilobzik <75563134+lofilobzik@users.noreply.github.com>
2022-01-10 18:08:33 +0300

    Parameters can now be hardcoded
    Defaults are:
    BRIGHTNESS = 0.5
    BLUR = 20

--- gdm-auto-blur.py
+++ gdm-auto-blur.py
@@ -9,17 +9,19 @@ from types import NoneType
 
 try:
     from PIL import Image, ImageFilter, ImageEnhance
-    from PIL import UnidentifiedImageError
 
 except ModuleNotFoundError as e:
     print(e)
     print('Please run \'pip3 install Pillow\'')
     sys.exit()
 
+BRIGHTNESS = 0.5
+BLUR = 20
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
-        usage='%(prog)s [-h] [-u] -i INPUT [-o OUTPUT] -br BRIGHTNESS -b BLUR [-p][-d]',
+        usage='%(prog)s [-h] [-u] -i INPUT [-o OUTPUT] [-br BRIGHTNESS] [-b BLUR] [-p] [-d]',
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=textwrap.dedent('''
         Sets GDM background image while blurring it and changing brightness.
@@ -33,9 +35,9 @@ def parse_args() -> argparse.Namespace:
                         required=('-u' not in sys.argv), help='specify the path of the image')
     parser.add_argument('-o', '--output', type=str,
                         help='specify output image directory (with a file name or without); input image directory by default')
-    parser.add_argument('-br', '--brightness', type=float, required=(
-        '-u' not in sys.argv), help='change brightness; from 0.00 to 1.00 and above')
-    parser.add_argument('-b', '--blur', type=float, required=('-u' not in sys.argv),
+    parser.add_argument('-br', '--brightness', type=float,
+                        help='change brightness; from 0.00 to 1.00 and above')
+    parser.add_argument('-b', '--blur', type=float,
                         help='change \'sigma\' parameter of gaussian blur (radius); from 0 to 50 and above')
     parser.add_argument('-p', '--preview', action='store_true',
                         help='preview an image without setting it as background (saves processed image)')
@@ -72,11 +74,15 @@ def main():
             output_path = output_path.with_suffix('.png')
 
         # Read amount of brightness and blur
-        brightness = args.brightness
+        if type(args.brightness) is NoneType:
+            brightness = BRIGHTNESS
+        else:
+            brightness = args.brightness
 
-        width = img.size[0]
-        const = args.blur / 1920
-        blur = round(width * const)
+        if type(args.blur) is NoneType:
+            blur = round(BLUR / 1920 * img.size[0])
+        else:
+            blur = round(args.blur / 1920 * img.size[0])
 
         # Apply filters
         img = img.filter(ImageFilter.GaussianBlur(radius=blur))
@@ -93,7 +99,9 @@ def main():
             return
 
         cmd = f'set-gdm-theme set -b \'{output_path}\''
-        print(f'Image saved as \'{output_path}\', running \'{cmd}\'')
+        print(f'Image saved as \'{output_path}\'')
+        print(f'Parameters: brightness: {brightness}, blur: {blur}')
+        print(f'Running \"{cmd}\"')
 
         os.system(cmd)
 
